a43b2b9a3b4c313e08413e967d44a7998f57d172
Oksana <puga.oksana@gmail.com>
2019-02-13 21:48:52 +0200

    comment box functioality

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,5 +1,32 @@
 import React from 'react';
 
-export default () => (
-    <div>Comment Box Component</div>
-);
+class CommentBox extends React.Component {
+    state = { comment: '' }
+
+    onChange = e => {
+        this.setState({ comment: e.target.value });
+    }
+
+    onSubmit = e => {
+        e.preventDefault();
+        this.setState({ comment: '' });
+    }
+
+    render() {
+        return (
+            <form>
+                <h4>Add a comment</h4>
+                <textarea
+                    value={this.state.comment}
+                    onChange={this.onChange} />
+                <div>
+                    <button onClick={this.onSubmit}>
+                        Submit Comment
+                    </button>
+                </div>
+            </form>
+        );
+    }
+}
+
+export default CommentBox;
