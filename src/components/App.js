ef96c2c1211251d0d52046db94b3093d8f76605e
Oksana <puga.oksana@gmail.com>
2019-02-13 18:35:25 +0200

    basic components setup

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import CommentBox from './CommentBox';
+import CommetsList from './CommentsList';
+
+export default () => (
+    <div>
+        <div>App Component</div>
+        <CommentBox />
+        <CommetsList />
+    </div>
+);
