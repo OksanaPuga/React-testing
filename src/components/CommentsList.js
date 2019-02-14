3f4e1b79e5a0977360d9ec4985319176f4150658
Oksana <puga.oksana@gmail.com>
2019-02-14 21:14:22 +0200

    added comments list component

--- src/components/CommentsList.js
+++ src/components/CommentsList.js
@@ -1,5 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-export default () => (
-    <div>Comment List Component</div>
-);
+class CommentsList extends React.Component {
+    render() {
+        return (
+            <ul>{this.renderList()}</ul>
+        );
+    }
+
+    renderList() {
+        return this.props.comments.map(comment => (
+            <li key={comment}>{comment}</li>
+        ));
+    }
+}
+
+const mapStateToProps = (state) => ({
+    comments: state.comments
+});
+
+export default connect(mapStateToProps)(CommentsList);
