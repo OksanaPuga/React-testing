42866a13666ad6c6ef670ebd08d8d46224abd864
Oksana <puga.oksana@gmail.com>
2019-02-14 20:26:29 +0200

    refactored provider to integrate redux into tests

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import reducers from 'reducers';
 
+import Root from 'Root';
 import App from './components/App';
 
-ReactDOM.render(
-    <Provider store={createStore(reducers)}>
-        <App />
-    </Provider>,
-    document.querySelector('#root')
-);
+ReactDOM.render(<Root><App /></Root>, document.querySelector('#root'));
